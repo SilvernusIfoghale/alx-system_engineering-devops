647e522f796e05932de7f22d267e4224e8e2595c
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2023-02-21 03:23:41 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character ot print
  *
  * Return: On Success 1
  */
-
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
